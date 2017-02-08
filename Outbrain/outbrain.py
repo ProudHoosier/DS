0d5e095667dee7189a09e89af50df0828903502e
Shashi <shashank@umail.iu.edu>
2017-02-08 13:52:47 -0500

    Update outbrain.py

--- Outbrain/outbrain.py
+++ Outbrain/outbrain.py
@@ -1,27 +1,26 @@
 import pandas as pd
 import numpy as np 
 
-reg = 10 #regularization
+regularization = 10
 eval = False
 
 train = pd.read_csv("clicks_train.csv")
 
-cnt = train[train.clicked==1].ad_id.value_counts()
-cntall = train.ad_id.value_counts()
+count = train[train.clicked == 1].ad_id.value_counts()
+toatlCount = train.ad_id.value_counts()
 del train
 
 def get_prob(k):
-    if k not in cnt:
+    if k not in count:
         return 0
-    print(cnt[k]/(float(cntall[k]) + reg))
-    return cnt[k]/(float(cntall[k]) + reg)
+    print(count[k]/(float(toatlCount[k]) + regularization))
+    return count[k]/(float(toatlCount[k]) + regularization)
 
-def srt(x):
+def arrangement(x):
     ad_ids = map(int, x.split())
-    ad_ids = sorted(ad_ids, key=get_prob, reverse=True)
-    #print('ad_ids', ad_ids)
-    return " ".join(map(str,ad_ids))
+    ad_ids = sorted(ad_ids, key = get_prob, reverse=True)
+    return " ".join(map(str, ad_ids))
    
-subm = pd.read_csv("sample_submission.csv") 
-subm['ad_id'] = subm.ad_id.apply(lambda x: srt(x))
-subm.to_csv("result.csv", index=False)
+result = pd.read_csv("sample_result.csv") 
+result['ad_id'] = result.ad_id.apply(lambda x: arrangement(x))
+result.to_csv("result.csv", index=False)
